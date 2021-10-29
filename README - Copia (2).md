1afee992de6f0ea47bf84540beed6f5b727595fc
Ricardo Silva <rigsjf@gmail.com>
2021-10-29 12:27:59 -0300

    Atualizacao

---
+++ README - Copia (2).md
@@ -0,0 +1,6 @@
+# Repositório do Desafio de Projeto sobre Git/GitHub da DIO
+Repositório criado para o Desafio de projeto sobre GitHub
+
+##  Links úteis
+
+[Sintaxe Básica Markdown](https://www.markdownguide.org/basic-syntax/)
---
+++ Revisao/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.Writeline("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() !+ "X")
+            {
+                 switch(opcaoUsuario)
+                 {
+                    case "1":
+                            //TODO: adicionar aluno
+                        break;
+                            //TODO: listar alunos
+                    case "2":
+                            //TODO: calcular media geral
+                        break;
+
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                 }
+                 
+                 Console.WriteLine("Informe a opção desejada:");
+                 Console.Writeline("1 - Inserir novo aluno");
+                 Console.WriteLine("2 - Listar alunos");
+                 Console.WriteLine("3 - Calcular média geral");
+                 Console.WriteLine("X - Sair");
+                 Console.WriteLine();
+                 
+                 opcaoUsuario = console.ReadLine();
+            }
+        }
+    }
+}
